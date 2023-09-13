bdbe63a9ead078fb513f6debc3734baa5b91d9e5
katarina.pavlovic <katarina.pavlovic@united.cloud>
2023-09-13 16:03:06 +0200

    Cleaned the code

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -7,8 +7,7 @@ class IndexController extends Controller
     public function indexAction()
     {
         $this->view->users = Users::find();
-        $message = "Hello from IndexController - test";
-        $this->view->message = $message;
+//        $this->view->message = $message;
 //        echo "Test";
     }
     public function registerAction()
